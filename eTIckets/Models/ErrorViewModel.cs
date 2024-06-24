66be09e48dd390d6a62eccba8bb50466257c74fc
USUARIO <USUARIO@DESKTOP-RD2VB0L>
2024-06-24 14:55:49 -0300

    Adicionar arquivos de projeto.

---
+++ eTIckets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTIckets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
